46a6398698746544c3ab4cea5780fc80f327c8ea
abhishek-tec123 <152301770+abhishek-tec123@users.noreply.github.com>
2024-04-28 09:47:52 +0530

    Delete local_chat directory

--- local_chat/constants.py
+++
@@ -1,7 +0,0 @@
-from chromadb.config import Settings 
-
-CHROMA_SETTINGS = Settings(
-        chroma_db_impl='duckdb+parquet',
-        persist_directory='db',
-        anonymized_telemetry=False
-)
