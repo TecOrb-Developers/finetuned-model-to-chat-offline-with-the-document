acda8009f632167210f4b95c0221c6e1eb92831f
abhishek-tec123 <152301770+abhishek-tec123@users.noreply.github.com>
2024-04-28 09:55:33 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,7 @@
+from chromadb.config import Settings 
+
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory='db',
+        anonymized_telemetry=False
+)
